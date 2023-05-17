b125def0caa619baa50af5ca5537a0872dd7985c
Chandankl <82862247+chandi7@users.noreply.github.com>
2023-05-18 00:15:59 +0530

    chore: Initial commit springboot

---
+++ src/main/java/com/example/springbootvehicledemo/SpringbootvehicledemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvehicledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootvehicledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootvehicledemoApplication.class, args);
+	}
+
+}
